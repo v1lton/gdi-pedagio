78db1f032ef6f86ae46826d2fcd18e9bf50b00ef
Wilton Ramos <wrs@cin.ufpe.br>
2022-03-29 20:52:25 -0300

    Cria tabela de Veiculo

---
+++ Entrega 1/Tabelas individuais/Veiculo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Veiculo (
+    placa VARCHAR2(7) NOT NULL,
+    tipo VARCHAR2(255) NOT NULL,
+    cpf_cliente VARCHAR2(11) NOT NULL,
+    CONSTRAINT veiculo_pk PRIMARY KEY (placa),
+    CONSTRAINT veiculo_fk FOREIGN KEY (cpf_cliente) REFERENCES Cliente(cpf)
+)
