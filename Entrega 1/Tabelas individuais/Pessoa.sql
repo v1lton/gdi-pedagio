e0cfc6330c9cce8e1d065e5451a0122b958bd536
Wilton Ramos <wrs@cin.ufpe.br>
2022-03-29 20:51:46 -0300

    Cria tabela de Pessoa

---
+++ Entrega 1/Tabelas individuais/Pessoa.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Pessoa (
+    cpf VARCHAR2(11) NOT NULL,
+    nascimento DATE NOT NULL,
+    nome VARCHAR2(255) NOT NULL,
+    sobrenome VARCHAR2(255) NOT NULL,
+    CONSTRAINT pessoa_pkey PRIMARY KEY (cpf)
+)
