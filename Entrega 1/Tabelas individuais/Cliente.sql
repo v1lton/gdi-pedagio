bfeef906ac2bac36991c4b8401d81596223d762b
Wilton Ramos <wrs@cin.ufpe.br>
2022-03-30 11:53:48 -0300

    Adiciona ponto e virgula em cliente

--- Entrega 1/Tabelas individuais/Cliente.sql
+++ Entrega 1/Tabelas individuais/Cliente.sql
@@ -2,4 +2,4 @@ CREATE TABLE Cliente ( --herda de pessoa
     cpf VARCHAR2(11) NOT NULL,
     CONSTRAINT cliente_pk PRIMARY KEY (cpf),
     CONSTRAINT pessoa_fk FOREIGN KEY (cpf) REFERENCES Pessoa(cpf)
-)
+);
