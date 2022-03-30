985cc723fe0b12357731bd1e76cb5a96e94063e2
Lucas Souza <38145232+lss8@users.noreply.github.com>
2022-03-30 20:57:24 -0300

    Update create_tables.sql

--- Entrega 1/Tabelas unificadas/create_tables.sql
+++ Entrega 1/Tabelas unificadas/create_tables.sql
@@ -18,7 +18,8 @@ CREATE TABLE Funcionario ( --herda de pessoa
     cargo VARCHAR2(255) NOT NULL,
     salario NUMBER NOT NULL CHECK (salario >= 1212.00),
     CONSTRAINT funcionario_pk PRIMARY KEY (cpf),
-    CONSTRAINT funcionario_fk FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario(cpf)
+    CONSTRAINT funcionario_fk FOREIGN KEY (cpf) REFERENCES Pessoa(cpf),
+    CONSTRAINT funcionario_sfk FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario(cpf)
 );
 
 CREATE TABLE Veiculo (
@@ -27,4 +28,65 @@ CREATE TABLE Veiculo (
     cpf_cliente VARCHAR2(11) NOT NULL,
     CONSTRAINT veiculo_pk PRIMARY KEY (placa),
     CONSTRAINT veiculo_fk FOREIGN KEY (cpf_cliente) REFERENCES Cliente(cpf)
-)
+);
+
+CREATE TABLE Pedagio (
+    nome VARCHAR2(255) NOT NULL,
+    endereco VARCHAR2(255) NOT NULL,
+    quant_cancelas NUMBER NOT NULL CHECK (quant_cancelas > 0),
+    CONSTRAINT pedagio_pk PRIMARY KEY (endereco)
+);
+
+CREATE TABLE Telefones (
+    cpf VARCHAR2(11) NOT NULL,
+    num_telefone VARCHAR2(11) NOT NULL,
+    CONSTRAINT telefone_pk PRIMARY KEY (cpf,num_telefone),
+    CONSTRAINT cpf_fk FOREIGN KEY (cpf) REFERENCES Pessoa(cpf)
+);
+
+CREATE TABLE Cancela (
+    endereco VARCHAR2(255) NOT NULL,
+    num_cancela NUMBER NOT NULL,
+    cpf_funcionario VARCHAR2(11) NOT NULL,
+    preco_tarifa NUMBER NOT NULL,
+    data_manutencao DATE NOT NULL,
+    status VARCHAR2(255) NOT NULL,
+    tipo_veiculo VARCHAR2(255) NOT NULL,
+    CONSTRAINT cancela_pk PRIMARY KEY (endereco, num_cancela),
+    CONSTRAINT cancela_fk FOREIGN KEY (endereco) REFERENCES Pedagio(endereco),
+    CONSTRAINT cancela_fk2 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf)
+);
+
+CREATE TABLE Monitora (
+	cpf_funcionario VARCHAR2(11) NOT NULL,
+	endereco VARCHAR2(255) NOT NULL,
+	num_cancela NUMBER NOT NULL,
+	inicio_monitoramento TIMESTAMP,
+	fim_monitoramento TIMESTAMP,
+	CONSTRAINT monitora_pk PRIMARY KEY (cpf_funcionario, endereco, num_cancela),
+	CONSTRAINT monitora_fk1  FOREIGN KEY (cpf_funcionario) REFERENCES  Funcionario(cpf),
+	CONSTRAINT monitora_fk2  FOREIGN KEY (endereco, num_cancela) REFERENCES Cancela(endereco, num_cancela)
+);
+
+CREATE TABLE Desconto (
+    porcentagem NUMBER NOT NULL,
+    codigo NUMBER NOT NULL,
+    cpf_cliente VARCHAR2(11) NOT NULL,
+    CONSTRAINT desconto_pk PRIMARY KEY (codigo),
+    CONSTRAINT desconto_fkey FOREIGN KEY (cpf_cliente) REFERENCES Cliente(cpf)
+);
+
+CREATE TABLE Pagamento (
+    cpf_cliente VARCHAR2(11) NOT NULL,
+    placa_veiculo VARCHAR2(7) NOT NULL,
+    endereco VARCHAR2(255) NOT NULL,
+    num_cancela NUMBER NOT NULL,
+    codigo_desconto NUMBER NOT NULL,
+    data_pagamento DATE NOT NULL,
+    forma_pagamento VARCHAR2(255) NOT NULL,
+    CONSTRAINT pagamento_pk PRIMARY KEY (cpf_cliente, placa_veiculo, endereco, num_cancela),
+    CONSTRAINT pagamento_fkey1 FOREIGN KEY (endereco, num_cancela) REFERENCES Cancela(endereco, num_cancela),
+    CONSTRAINT pagamento_fkey2 FOREIGN KEY (codigo_desconto) REFERENCES Desconto(codigo),
+    CONSTRAINT pagamento_fkey3 FOREIGN KEY (cpf_cliente) REFERENCES Cliente(cpf),
+    CONSTRAINT pagamento_fkey4 FOREIGN KEY (placa_veiculo) REFERENCES Veiculo(placa)
+);
