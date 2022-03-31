4edfe5f6e9ba158beab9d568304784f3d09f5406
Lucas Souza <38145232+lss8@users.noreply.github.com>
2022-03-31 20:27:48 -0300

    Update Funcionario.sql

--- Entrega 1/Tabelas individuais/Funcionario.sql
+++ Entrega 1/Tabelas individuais/Funcionario.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Funcionario ( --herda de pessoa
     cpf VARCHAR2(11) NOT NULL,
     cpf_supervisor VARCHAR2(11) NOT NULL,
+    endereco VARCHAR2(255) NOT NULL,
     cargo VARCHAR2(255) NOT NULL,
     salario NUMBER NOT NULL CHECK (salario >= 1212.00),
     CONSTRAINT funcionario_pk PRIMARY KEY (cpf),
-    CONSTRAINT funcionario_fk FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario(cpf)
+    CONSTRAINT funcionario_fk1 FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario(cpf),
+    CONSTRAINT funcionario_fk2 FOREIGN KEY (endereco) REFERENCES Pedagio(endereco)
 );
