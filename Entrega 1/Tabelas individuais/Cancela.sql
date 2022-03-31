c9fe4f2b843eec8cc6d446387f1b67e46e1bc464
Lucas Souza <38145232+lss8@users.noreply.github.com>
2022-03-31 19:49:56 -0300

    Update Cancela.sql

--- Entrega 1/Tabelas individuais/Cancela.sql
+++ Entrega 1/Tabelas individuais/Cancela.sql
@@ -1,12 +1,10 @@
 CREATE TABLE Cancela (
     endereco VARCHAR2(255) NOT NULL,
     num_cancela NUMBER NOT NULL,
-    cpf_funcionario VARCHAR2(11) NOT NULL,
     preco_tarifa NUMBER NOT NULL,
     data_manutencao DATE NOT NULL,
     status VARCHAR2(255) NOT NULL,
     tipo_veiculo VARCHAR2(255) NOT NULL,
     CONSTRAINT cancela_pk PRIMARY KEY (endereco, num_cancela),
-    CONSTRAINT cancela_fk FOREIGN KEY (endereco) REFERENCES Pedagio(endereco),
-    CONSTRAINT cancela_fk2 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf)
+    CONSTRAINT cancela_fk FOREIGN KEY (endereco) REFERENCES Pedagio(endereco)
 );
