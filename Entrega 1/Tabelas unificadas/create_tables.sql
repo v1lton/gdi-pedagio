33d2c98ec8af23420b2c02ce8e50fa4c2a7afeed
Lucas Souza <38145232+lss8@users.noreply.github.com>
2022-03-31 20:28:27 -0300

    Update create_tables.sql

--- Entrega 1/Tabelas unificadas/create_tables.sql
+++ Entrega 1/Tabelas unificadas/create_tables.sql
@@ -15,11 +15,12 @@ CREATE TABLE Cliente ( --herda de pessoa
 CREATE TABLE Funcionario ( --herda de pessoa
     cpf VARCHAR2(11) NOT NULL,
     cpf_supervisor VARCHAR2(11) NOT NULL,
+    endereco VARCHAR2(255) NOT NULL,
     cargo VARCHAR2(255) NOT NULL,
     salario NUMBER NOT NULL CHECK (salario >= 1212.00),
     CONSTRAINT funcionario_pk PRIMARY KEY (cpf),
-    CONSTRAINT funcionario_fk FOREIGN KEY (cpf) REFERENCES Pessoa(cpf),
-    CONSTRAINT funcionario_sfk FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario(cpf)
+    CONSTRAINT funcionario_fk1 FOREIGN KEY (cpf_supervisor) REFERENCES Funcionario(cpf),
+    CONSTRAINT funcionario_fk2 FOREIGN KEY (endereco) REFERENCES Pedagio(endereco)
 );
 
 CREATE TABLE Veiculo (
