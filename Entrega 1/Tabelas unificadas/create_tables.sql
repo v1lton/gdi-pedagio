6dec3218ac3ed8372fef541ad09c7e2f43c40207
thaisvcouto <55455993+thaisvcouto@users.noreply.github.com>
2022-04-04 21:15:00 -0300

    Update create_tables.sql
    Mudando chaves primárias de Monitora

--- Entrega 1/Tabelas unificadas/create_tables.sql
+++ Entrega 1/Tabelas unificadas/create_tables.sql
@@ -60,9 +60,9 @@ CREATE TABLE Monitora (
 	cpf_funcionario VARCHAR2(11) NOT NULL,
 	endereco VARCHAR2(255) NOT NULL,
 	num_cancela NUMBER NOT NULL,
-	inicio_monitoramento TIMESTAMP,
-	fim_monitoramento TIMESTAMP,
-	CONSTRAINT monitora_pk PRIMARY KEY (cpf_funcionario, endereco, num_cancela),
+	inicio_monitoramento TIMESTAMP NOT NULL,
+	fim_monitoramento TIMESTAMP NOT NULL,
+	CONSTRAINT monitora_pk PRIMARY KEY (cpf_funcionario, endereco, num_cancela, inicio_monitoramento, fim_monitoramento),
 	CONSTRAINT monitora_fk1  FOREIGN KEY (cpf_funcionario) REFERENCES  Funcionario(cpf),
 	CONSTRAINT monitora_fk2  FOREIGN KEY (endereco, num_cancela) REFERENCES Cancela(endereco, num_cancela)
 );
