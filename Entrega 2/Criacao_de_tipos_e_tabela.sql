04503945d83084ece164ba80ea605b1e588668e7
thaisvcouto <55455993+thaisvcouto@users.noreply.github.com>
2022-05-03 22:51:04 -0300

    Update Criacao_de_tipos_e_tabela.sql
    Organizando código e dividindo em criação de tipos e de tabelas

--- Entrega 2/Criacao_de_tipos_e_tabela.sql
+++ Entrega 2/Criacao_de_tipos_e_tabela.sql
@@ -1,18 +1,22 @@
--- PESSOA --
--- NOT FINAL: permite criar subtipos -- 
--- NOT INSTANTIABLES: não pode ter instâncias de objetos criadas em tabelas --
--- CLIENTE E FUNCIONÁRIO HERDAM DE PESSOA --
+--////// CRIAÇÃO DE TIPOS ////////--
+
+-- TIPO TELEFONE
 CREATE OR REPLACE TYPE tp_telefone AS OBJECT (
     telefone VARCHAR (14) 
 );
 
 /
 
+-- ARRAY DE TELEFONES 
 -- Checklist: 19
 CREATE OR REPLACE TYPE tp_varray_telefone AS VARRAY (5) OF tp_telefone;
 
 /
 
+
+-- TIPO PESSOA
+-- NOT FINAL: permite criar subtipos
+-- NOT INSTANTIABLES: não pode ter instâncias de objetos criadas em tabelas
 -- Checklist: 1, 3, 10
 CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
 
@@ -27,16 +31,19 @@ CREATE OR REPLACE TYPE tp_pessoa AS OBJECT (
 
 /
 
--- CLIENTE --
--- HERDA DE PESSOA --
+-- TIPO CLIENTE
+-- Herda de Pessoa
 CREATE OR REPLACE TYPE tp_cliente UNDER tp_pessoa (
 
     FINAL MAP MEMBER FUNCTION qntd_telefones RETURN NUMBER
 
 );
+
 /
+
+-- CORPO DOS MÉTODOS PARA tp_cliente
+-- qntd_telefones: retorna a quantidade de telefones cadastrados por cliente
 -- Checklist 6, 9
--- QNTD_TELEFONES: FINAL MAP MEMBER QUE RETORNA A QUANTIDADE DE TELEFONES CADASTRADOS NO RESTAURANTE --
 CREATE OR REPLACE TYPE BODY tp_cliente AS 
 FINAL MAP MEMBER FUNCTION qntd_telefones RETURN NUMBER IS
 selfCnt NUMBER;
@@ -49,8 +56,8 @@ END;
 
 /
 
--- FUNCIONÁRIO --
--- HERDA DE PESSOA --
+-- TIPO FUNCIONÁRIO
+-- Herda de Pessoa
 -- Checklist: 8, 11, 15
 CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
 
@@ -65,6 +72,135 @@ CREATE OR REPLACE TYPE tp_funcionario UNDER tp_pessoa (
 
 /
 
+-- CORPO DOS MÉTODOS PARA tp_funcionario
+-- salarioAnual: member function que calcula o salário anual do funcionário
+-- comparaSalario: order member function que compara salários (se SELF>X, resultado é positivo, se SELF=X, resultado é 0 e se SELF<X, resultado é negativo)
+-- print_info: overriding member procedure que imprime as informações do funcionário
+-- constructor: padrão
+-- Checklist: 2, 4
+CREATE OR REPLACE TYPE BODY tp_funcionario AS
+
+MEMBER FUNCTION salarioAnual RETURN NUMBER IS
+    BEGIN
+        RETURN salario*12;
+    END;
+-- Checklist: 5
+ORDER MEMBER FUNCTION comparaSalario (X tp_funcionario) RETURN INTEGER IS
+    BEGIN
+        RETURN SELF.salario - X.salario;
+    END;
+OVERRIDING MEMBER PROCEDURE print_info IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE(nome);
+        DBMS_OUTPUT.PUT_LINE(cpf);
+        DBMS_OUTPUT.PUT_LINE(cpf_supervisor);
+        DBMS_OUTPUT.PUT_LINE(cargo);
+        DBMS_OUTPUT.PUT_LINE(salario);
+    END;
+-- Checklist: 7
+CONSTRUCTOR FUNCTION tp_entregador (x1 tp_entregador) RETURN SELF AS RESULT IS
+    BEGIN
+        cpf := x1.cpf; 
+        nome := x1.nome; 
+        sobrenome := x1.sobrenome; 
+        nascimento := x1.nascimento;
+        telefone := x1.telefone; 
+        cpf_supervisor := x1.cpf_supervisor; 
+        endereco := x1.endereco;
+        cargo := x1.cargo; 
+        salario := x1.salario; 
+        RETURN; 
+    END;
+
+END;
+
+/
+
+-- TIPO VEÍCULO
+CREATE OR REPLACE TYPE tp_veiculo AS OBJECT (
+
+    cpf_cliente REF tp_cliente,
+    placa VARCHAR2(7),
+    tipo VARCHAR2(255)
+
+);
+
+/
+
+-- TIPO DESCONTO
+CREATE OR REPLACE TYPE tp_desconto AS OBJECT (
+
+    porcentagem NUMBER,
+    codigo NUMBER,
+    cpf_cliente REF tp_cliente
+
+);
+
+/
+
+-- TIPO CANCELA
+CREATE OR REPLACE TYPE tp_cancela AS OBJECT (
+
+    num_cancela NUMBER,
+    preco_tarica NUMBER,
+    data_manutencao DATE,
+    status_cancela VARCHAR2 (10),
+    tipo_veiculo VARCHAR2 (10)
+
+);
+
+/
+
+-- TIPO PEDAGIO
+CREATE OR REPLACE TYPE tp_pedagio AS OBJECT (
+
+    nome VARCHAR2 (20),
+    endereco VARCHAR2 (100),
+    quant_cancelas NUMBER,
+    cancelas tp_nt_cancelas
+
+);
+
+/
+
+-- TIPO MONITORA
+CREATE OR REPLACE TYPE tp_monitora AS OBJECT (
+
+    cpf_funcionario REF tp_funcionario,
+    endereco REF tp_pedagio,
+    num_cancela NUMBER,
+    inicio_monitoramento TIMESTAMP,
+    fim_monitoramento TIMESTAMP
+
+);
+
+/
+
+-- TIPO PAGAMENTO
+CREATE OR REPLACE TYPE tp_pagamento AS OBJECT (
+
+    cpf_cliente REF tp_cliente,
+    placa_veiculo REF tp_veiculo,
+    endereco REF tp_pedagio,
+    num_cancela NUMBER,
+    codigo_desconto REF tp_desconto,
+    data_pagamento TIMESTAMP,
+    forma_pagamento VARCHAR2 (10)
+
+);
+
+/
+
+-- ALTER TYPE + ADD: adiciona atributo email em pessoa
+-- CASCADE: propaga a mudança para tipos dependentes de tp_pessoa
+-- Checklist: 12
+ALTER TYPE tp_pessoa ADD ATTRIBUTE (naturalidade VARCHAR2(20)) CASCADE;
+
+/
+
+--////// CRIAÇÃO DE TABELAS ////////--
+
+-- TABELA CLIENTE
 -- Checklist 13
 CREATE TABLE tb_cliente OF tp_cliente (
 
@@ -76,6 +212,8 @@ CREATE TABLE tb_cliente OF tp_cliente (
 );
 
 /
+
+-- TABELA FUNCIONÁRIO
 -- Checklist: 14
 CREATE TABLE tb_funcionario OF tp_funcionario (
 
@@ -91,67 +229,9 @@ CREATE TABLE tb_funcionario OF tp_funcionario (
 
 );
 
--- Checklist: 12
--- ADICIONANDO EMAIL EM PESSOA --
--- CASCADE: PROPAGA A MUDANÇA PARA TIPOS DEPENDENTES -- 
-ALTER TYPE tp_pessoa ADD ATTRIBUTE (naturalidade VARCHAR2(20)) CASCADE;
-
 /
 
--- CORPO DOS MÉTODOS PARA tp_funcionario --
--- Checklist: 2, 4
-CREATE OR REPLACE TYPE BODY tp_funcionario IS
-MEMBER FUNCTION salarioAnual RETURN NUMBER IS
-BEGIN
-RETURN salario*12;
-END;
-
--- Checklist: 5
-ORDER MEMBER FUNCTION comparaSalario (X tp_funcionario)
-RETURN INTEGER IS
-BEGIN
-RETURN SELF.salario - X.salario;
-END;
-
-OVERRIDING MEMBER PROCEDURE print_info IS
-BEGIN
-    DBMS_OUTPUT.PUT_LINE(nome);
-    DBMS_OUTPUT.PUT_LINE(cpf);
-    DBMS_OUTPUT.PUT_LINE(cpf_supervisor);
-    DBMS_OUTPUT.PUT_LINE(cargo);
-    DBMS_OUTPUT.PUT_LINE(salario);
-END;
-
--- Checklist: 7
-CONSTRUCTOR FUNCTION tp_entregador (x1 tp_entregador) RETURN SELF AS RESULT IS
-    BEGIN
-        cpf := x1.cpf; 
-        nome := x1.nome; 
-        sobrenome := x1.sobrenome; 
-        nascimento := x1.nascimento;
-        telefone := x1.telefone; 
-        cpf_supervisor := x1.cpf_supervisor; 
-        endereco := x1.endereco;
-        cargo := x1.cargo; 
-        salario := x1.salario; 
-        RETURN; 
-    END;
-END;
-
-/
-
-
--- VEÍCULO --
-CREATE OR REPLACE TYPE tp_veiculo AS OBJECT (
-
-    cpf_cliente REF tp_cliente,
-    placa VARCHAR2(7),
-    tipo VARCHAR2(255)
-
-);
-
-/
-
+-- TABELA VEÍCULO
 CREATE TABLE tb_veiculo OF tp_veiculo (
 
     placa PRIMARY KEY,
@@ -162,14 +242,7 @@ CREATE TABLE tb_veiculo OF tp_veiculo (
 
 /
 
--- DESCONTO --
-CREATE OR REPLACE TYPE tp_desconto AS OBJECT (
-
-    porcentagem NUMBER,
-    codigo NUMBER,
-    cpf_cliente REF tp_cliente
-
-);
+-- TABELA DESCONTO
 -- Checklist: 16
 CREATE TABLE tb_desconto OF tp_desconto (
 
@@ -181,30 +254,13 @@ CREATE TABLE tb_desconto OF tp_desconto (
 
 /
 
--- CANCELA --
-CREATE OR REPLACE TYPE tp_cancela AS OBJECT (
-
-    num_cancela NUMBER,
-    preco_tarica NUMBER,
-    data_manutencao DATE,
-    status_cancela VARCHAR2 (10),
-    tipo_veiculo VARCHAR2 (10)
-
-);
+-- TABELA CANCELA
 -- Checklist 20
 CREATE OR REPLACE TYPE tp_nt_cancelas AS TABLE OF tp_cancela;
 
+/
 
--- PEDAGIO --
-CREATE OR REPLACE TYPE tp_pedagio AS OBJECT (
-
-    nome VARCHAR2 (20),
-    endereco VARCHAR2 (100),
-    quant_cancelas NUMBER,
-    cancelas tp_nt_cancelas
-
-);
-
+--TABELA PEDAGIO
 CREATE TABLE tb_pedagio OF tp_desconto (
 
     endereco PRIMARY KEY ,
@@ -215,17 +271,7 @@ CREATE TABLE tb_pedagio OF tp_desconto (
 
 /
 
--- MONITORA --
-CREATE OR REPLACE TYPE tp_monitora AS OBJECT (
-
-    cpf_funcionario REF tp_funcionario,
-    endereco REF tp_pedagio,
-    num_cancela NUMBER,
-    inicio_monitoramento TIMESTAMP,
-    fim_monitoramento TIMESTAMP
-
-);
-
+-- TABELA MONITORA
 CREATE TABLE tb_monitora OF tp_monitora (
 
     cpf_funcionario WITH ROWID REFERENCES tb_funcionario,
@@ -236,19 +282,9 @@ CREATE TABLE tb_monitora OF tp_monitora (
 
 );
 
--- PAGAMENTO --
-CREATE OR REPLACE TYPE tp_pagamento AS OBJECT (
+/
 
-    cpf_cliente REF tp_cliente,
-    placa_veiculo REF tp_veiculo,
-    endereco REF tp_pedagio,
-    num_cancela NUMBER,
-    codigo_desconto REF tp_desconto,
-    data_pagamento TIMESTAMP,
-    forma_pagamento VARCHAR2 (10)
-
-);
-
+-- TABELA PAGAMENTO
 CREATE TABLE tb_pagamento OF tp_pagamento (
 
     cpf_cliente WITH ROWID REFERENCES tp_cliente,
