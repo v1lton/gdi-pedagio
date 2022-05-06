e1e8ede7d37a402f5b7206bf0fc245d57583fd3b
Wilton Ramos <wrs@cin.ufpe.br>
2022-05-05 21:36:10 -0300

    Povamento de veículos e desconto

--- Entrega 2/povoamento.sql
+++ Entrega 2/povoamento.sql
@@ -8,7 +8,39 @@ CREATE SEQUENCE id INCREMENT by 1 START WITH 1;
 -- CLIENTE
 -- FUNCIONARIO
 -- VEICULO
+INSERT INTO tb_veiculo VALUES (
+    tp_veiculo(
+        (SELECT REF(R) FROM tb_cliente R WHERE R.cpf = '001'),
+        'AAA1234',
+        'pessoal'
+    )
+);
+/
+INSERT INTO tb_veiculo VALUES (
+    tp_veiculo(
+        (SELECT REF(R) FROM tb_cliente R WHERE R.cpf = '005'),
+        'BBB2021',
+        'pessoal'
+    )
+);
+/
 -- DESCONTO
+INSERT INTO tb_desconto VALUES (
+    tp_desconto(
+        '10',
+        codigo.NEXTVAL,
+        (SELECT REF(R) FROM tb_cliente R WHERE R.cpf = '001')
+    )
+);
+/
+INSERT INTO tb_desconto VALUES (
+    tp_desconto(
+        '10',
+        codigo.NEXTVAL,
+        (SELECT REF(R) FROM tb_cliente R WHERE R.cpf = '005')
+    )
+);
+/
 -- CANCELA
 -- PEDAGIO
 -- MONITORA
