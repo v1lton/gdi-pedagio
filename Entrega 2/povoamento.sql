eff9febb2f8b03fb72045ba9bdd092bf42c8bd1d
Thayná Cavalcante <48687864+tecs2000@users.noreply.github.com>
2022-05-05 21:18:27 -0300

    Update povoamento.sql

--- Entrega 2/povoamento.sql
+++ Entrega 2/povoamento.sql
@@ -13,3 +13,37 @@ CREATE SEQUENCE id INCREMENT by 1 START WITH 1;
 -- PEDAGIO
 -- MONITORA
 -- PAGAMENTO
+
+-- Povoamento de tb_pedagio e tp_nt_cancela
+INSERT INTO tb_pedagio VALUES (
+    tp_pedagio(
+        'Valerium',
+        'Rua das Ninfas, número 70',
+        '3',
+        tp_nt_cancela(
+            tp_cancela(
+                1,
+                5.50,
+                to_date('05/12/2020', 'dd/mm/yy'),
+                'ativa',
+                'pessoal'
+            ),
+            tp_cancela(
+                2,
+                7.50,
+                to_date('05/12/2020', 'dd/mm/yy'),
+                'desativada',
+                'coletivo'
+            ),
+            tp_cancela(
+                3,
+                5.50,
+                to_date('05/12/2020', 'dd/mm/yy'),
+                'ativa',
+                'pessoal'
+            )
+        )
+    )
+);
+
+/
