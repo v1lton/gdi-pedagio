a029997c2c7671b6d1cfc2b74f306a5f625df798
thaisvcouto <55455993+thaisvcouto@users.noreply.github.com>
2022-05-05 21:23:38 -0300

    Povoamento de cliente e funcionário

--- Entrega 2/povoamento.sql
+++ Entrega 2/povoamento.sql
@@ -100,3 +100,147 @@ INSERT INTO tb_pedagio VALUES (
 );
 
 /
+
+-- POVOAMENTO CLIENTE
+
+INSERT INTO tb_cliente VALUES (
+
+    tp_cliente(
+        '001',
+        to_date('18/06/1992', 'dd/mm/yy'),
+        'Rogério', 
+        'Gouveia',
+        tp_arr_telefone(tp_telefone('81900000001'), tp_telefone('81900000011')),
+        'Recife-PE'
+    )
+
+);
+
+/
+
+INSERT INTO tb_cliente VALUES (
+
+    tp_cliente(
+        '002',
+        to_date('16/02/1999', 'dd/mm/yy'),
+        'Fabricia',
+        'Galeão'
+        tp_arr_telefone(tp_telefone('81900000002')),
+        'Recife-PE'
+    )
+
+);
+
+/
+
+INSERT INTO tb_cliente VALUES (
+
+    tp_cliente(
+        '005',
+        to_date('30/03/1974', 'dd/mm/yy'),
+        'Socorro', 
+        'Antunes'
+        tp_arr_telefone(tp_telefone('81900000005')),
+        'Recife-PE'
+    )
+
+);
+
+/
+
+-- POVOAMENTO FUNCIONÁRIO
+
+INSERT INTO tb_funcionario VALUES (
+
+    tp_funcionario(
+        '001',
+        to_date('18/06/1992', 'dd/mm/yy'),
+        'Rogério', 
+        'Gouveia',
+        tp_arr_telefone(tp_telefone('81900000001'), tp_telefone('81900000011'))
+        'Recife-PE',
+        '001', 
+        SELECT REF(E) FROM tb_pedagio WHERE E.endereco = 'Rua Amelia 65',
+        'gerente', 
+        '12000'
+    )
+
+);
+
+/
+
+INSERT INTO tb_funcionario VALUES (
+
+    tp_funcionario(
+        '006', 
+        to_date('31/12/2000', 'dd/mm/yy'),
+        'Samuel', 
+        'Fernandes',
+        tp_arr_telefone(tp_telefone('81900000006')),
+        'Recife-PE',
+        '001', 
+        SELECT REF(E) FROM tb_pedagio WHERE E.endereco = 'Rua Amelia 65',
+        'coordenador', 
+        '7000'
+    )
+
+);
+
+/
+
+INSERT INTO tb_funcionario VALUES (
+
+    tp_funcionario(
+        '007', 
+        to_date('12/04/1993', 'dd/mm/yy'),
+        'James', 
+        'Bond',
+        tp_arr_telefone(tp_telefone('81900000007')),
+        'Recife-PE',
+        '006', 
+        SELECT REF(E) FROM tb_pedagio WHERE E.endereco = 'Rua Amelia 65',
+        'atendente', 
+        '1500'
+    )
+
+);
+
+/
+
+INSERT INTO tb_funcionario VALUES (
+
+    tp_funcionario(
+        '010', 
+        to_date('10/06/1992', 'dd/mm/yy'),
+        'Felipe', 
+        'Santos',
+        tp_arr_telefone(tp_telefone('81900000010')),
+        'Recife-PE',
+        '006', 
+        SELECT REF(E) FROM tb_pedagio WHERE E.endereco = 'Rua Amelia 65',
+        'atendente', 
+        '1500'
+    )
+
+);
+
+/
+
+INSERT INTO tb_funcionario VALUES (
+
+    tp_funcionario(
+        '011', 
+        to_date('08/01/1988', 'dd/mm/yy'),
+        'João', 
+        'Oliveira',
+        tp_arr_telefone(tp_telefone('81900000111')),
+        'Recife-PE',
+        '006', 
+        SELECT REF(E) FROM tb_pedagio WHERE E.endereco = 'Rua Amelia 65',
+        'atendente', 
+        '1500'
+    )
+
+);
+
+/
